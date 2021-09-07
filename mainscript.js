b514148aa42ff6bcb9055ef82190103ebf892e6b
ABU <syedabuanwar@gamil.com>
2021-09-07 16:01:51 +0530

    all done for second page

---
+++ mainscript.js
@@ -0,0 +1,19 @@
+// code for dropdown start
+
+function f1(){
+    var x = document.getElementById("dropdown");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+
+      document.addEventListener('mouseup', function(e) {
+        var container = document.getElementById('dropdown');
+        if (!container.contains(e.target)) {
+            container.style.display = 'none';
+        }
+      })
+
+// code for dropdown end
